f14cf8f17a4865f0adfb6dbee273ff74f7c63e5d
iwaniuk <sp6mi@protonmail.com>
2017-09-05 18:23:22 +0200

    add comments to dbhandler, add sceleton to qso manager class

--- dbhandler/dbhandler.py
+++ dbhandler/dbhandler.py
@@ -1,33 +1,60 @@
 import sqlite3
 
 class DataBaseHandler:
+    '''
+    Default DB file name is test.db. Correct file name
+    must be provided in constructor
+    '''
 
     dbfile = 'test.db'
 
 
     def __init__(self, dbfile):
+        '''
+        constructor
+        '''
+
         self.dbfile = dbfile
 
 
     def open_DB(self, dbfile=None):
+        '''
+        Open connection to DB. If file name to DB
+        is not given then use default test.db name
+        '''
+
         if dbfile is not None:
             self.dbfile = dbfile
+        
         connection = sqlite3.connect(dbfile)
+        
         return connection
 
 
     def execute_query(self, dbfile, query):
+        '''
+        Executing query in DB. Both DB and query are params
+        to this method
+        '''
+
         dbconnection = self.open_DB(dbfile)
         cursor = dbconnection.cursor()
 
         try:
             cursor.execute(query)
             data = cursor.fetchall()
+
+            return data
+            
         except:
             raise sqlite3.Error("Can't execute query")
 
 
     def create_table(self, dbfile):
+        '''
+        Create table in database.
+        '''
+
         query = """
         CREATE TABLE qso (
         id integer PRIMARY KEY,
@@ -50,5 +77,8 @@ class DataBaseHandler:
             cursor = dbconnection.cursor()
             cursor.execute(query)
             result = cursor.fetchall()
+
+            return result
+
         except:
-            pass
+            raise sqlite3.Error("Table creation failed. Try again")
---
+++ qsomanager/qsomanager.py
@@ -0,0 +1,20 @@
+class QsoManager:
+	'''
+	Main class to handle all operation with single QSO
+	from this level we will execute some of DB operations
+	'''
+
+	def __init__(self):
+		pass
+
+	def add_qso(self):
+		pass
+
+	def edit_qso(self):
+		pass
+
+	def search_qso(self):
+		pass
+
+	def remove_qso(self):
+		pass
